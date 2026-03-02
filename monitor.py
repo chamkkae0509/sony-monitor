01a1e2201ee43d268f4d8305e4e4a2fa6a11c236
chamkkae0509 <chamkkae0509@gmail.com>
2026-03-02 13:32:37 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import time
 
 URL = "https://store.sony.co.kr/product-view/132237901"
@@ -8,21 +7,12 @@ print("모니터링 시작", flush=True)
 
 while True:
     res = requests.get(URL)
-    soup = BeautifulSoup(res.text, "html.parser")
+    html = res.text
 
-    button = soup.select_one("a.btn_style")
-
-    if button:
-        href = button.get("href")
-        text = button.text.strip()
-
-        print(f"현재 상태: {text}", flush=True)
-
-        if href and href != "#none":
-            print("🔥 구매 가능 상태!", flush=True)
-            break
-
+    if "/order/sheet" in html:
+        print("🔥 구매 가능 상태!", flush=True)
+        break
     else:
-        print("버튼 찾기 실패", flush=True)
+        print("아직 품절 상태", flush=True)
 
     time.sleep(10)
