fe9b2a333760a7c80823968a29b78209220b6c67
chamkkae0509 <chamkkae0509@gmail.com>
2026-03-02 13:03:48 +0900

    Add files via upload

---
+++ monitor.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+URL = "https://store.sony.co.kr/product-view/132237901"
+
+def check_button():
+    res = requests.get(URL)
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    button = soup.select_one("a.btn_style")
+
+    if button:
+        text = button.text.strip()
+        link = button.get("href")
+
+        print("버튼 상태:", text)
+
+        if link == "/order/sheet":
+            print("🔥 구매 가능!")
+            return True
+
+    return False
+
+
+while True:
+    if check_button():
+        print("구매 링크 생성됨!")
+        break
+
+    time.sleep(10)
