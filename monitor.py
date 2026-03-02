942606a89d38d988d8ddfc4928a2abef003fcbae
chamkkae0509 <chamkkae0509@gmail.com>
2026-03-02 13:51:01 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,11 +1,11 @@
 import requests
 import time
+import os
 
 URL = "https://store.sony.co.kr/product-view/132237901"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-
 
 def send_telegram(msg):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -14,18 +14,3 @@ def send_telegram(msg):
         "text": msg
     }
     requests.post(url, data=data)
-
-print("모니터링 시작", flush=True)
-
-while True:
-    res = requests.get(URL)
-    html = res.text
-
-    if "/order/sheet" in html:
-        print("🔥 구매 가능 상태!", flush=True)
-        send_telegram("🔥 소니 제품 구매 가능!\n바로 접속하세요:\n" + URL)
-        break
-    else:
-        print("아직 품절 상태", flush=True)
-
-    time.sleep(5)
