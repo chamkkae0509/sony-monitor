5edaaae984b59f02dba1f13fce11950706dc774a
chamkkae0509 <chamkkae0509@gmail.com>
2026-03-02 14:00:47 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -5,7 +5,7 @@ import time
 import os
 
 
-URL = "https://store.sony.co.kr/product-view/132237901"
+URL = "https://store.sony.co.kr/product-view/131844793"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
