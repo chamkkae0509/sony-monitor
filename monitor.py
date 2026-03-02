72486fc2fda697aa0405e1a3c395cbae8b38a3f7
chamkkae0509 <chamkkae0509@gmail.com>
2026-03-02 14:08:46 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,10 +1,7 @@
 import requests
 import time
-import requests
-import time
 import os
 from bs4 import BeautifulSoup
-
 
 URL = "https://store.sony.co.kr/product-view/131844793"
 
@@ -25,15 +22,14 @@ while True:
     res = requests.get(URL)
     html = res.text
 
+    soup = BeautifulSoup(html, "html.parser")
+    button = soup.find("a", class_="btn_style direct")
 
+    if button:
+        print("🔥 구매 가능!", flush=True)
+        send_telegram("🔥 재입고!\n" + URL)
+        break
+    else:
+        print("아직 품절 상태", flush=True)
 
-soup = BeautifulSoup(html, "html.parser")
-
-button = soup.find("a", class_="btn_style direct")
-
-if button:
-    print("🔥 구매 가능!")
-else:
-    print("아직 품절 상태")
-    
     time.sleep(5)
